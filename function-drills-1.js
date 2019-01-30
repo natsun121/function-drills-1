ec09ddc89baf4d2b12f5406d7d3ad337ce8b8d88
natsun121 <natsun121@gmail.com>
2019-01-30 10:15:00 -0800

    added error blocks

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,18 +1,28 @@
 function getYearOfBirth(age) {
-  if(age < 0) {
-    throw new Error('Age can not be negative');
-  }
   return 2018 - age;
 }
 
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid.');
+  }
+
+  if (age < 0) {
+    throw new Error('Age can not be negative.');
+  }
+
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number.')
+  }
+
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 try {
-  const greeting1 = createGreeting('Natalie', -24);
-} catch (error) {
-  console.log(error);
+  const greeting1 = createGreeting('Natalie', 24);
+  console.log(greeting1);
+} catch (e) {
+  console.log(e.message);
 }
