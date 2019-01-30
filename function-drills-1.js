3627a6d4fe6229ea0d77340a7b23e253a0445f9c
natsun121 <natsun121@gmail.com>
2019-01-30 09:20:37 -0800

    added return statement

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,6 +1,7 @@
 
 function createGreeting() {
-  console.log(`Hi, my name is Natalie and I'm 24 years old`);
+  return `Hi, my name is Natalie and I'm 24 years old`;
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
